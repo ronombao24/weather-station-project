167515b6bed2bbf21730e30758d67c1a8b76b39b
sarthakthakkar <71234720+sarthakthakkar@users.noreply.github.com>
2022-01-12 17:44:15 -0500

    Add files via upload

---
+++ Software App/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.weatherdetector;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button temp_btn = findViewById(R.id.temperature_button);
+        Button humid_btn = findViewById(R.id.humidity_button);
+        Button air_press_btn = findViewById(R.id.air_pressure_button);
+        Button reading_btn = findViewById(R.id.reading_button);
+        Button air_quality = findViewById(R.id.air_quality_button);
+
+        temp_btn.setOnClickListener (view -> startActivity(new Intent(MainActivity.this, TemperatureReader.class)));
+
+        humid_btn.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, HumidityReader.class)));
+
+        air_press_btn.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, AirPressureReader.class)));
+
+        reading_btn.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, ReadingActivity.class)));
+
+        air_quality.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, AirQualityReader.class)));
+    }
+}
