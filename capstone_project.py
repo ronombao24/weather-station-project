49f6480b3809ee0866f778ba8f2263924d730def
TJStave <n01286513@humbermail.ca>
2022-01-17 13:28:25 -0500

    missing sensors error handling

--- capstone_project.py
+++ capstone_project.py
@@ -24,27 +24,61 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 #SENSOR DECLARATIONS
+bmpMissing = False
+ahtMissing = False
+lpsMissing = False
+sgpMissing = False
+
 i2c = busio.I2C(board.SCL, board.SDA)
-bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
-aht20 = adafruit_ahtx0.AHTx0(i2c)
-lps22 = adafruit_lps2x.LPS22(i2c)
-sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
+try:
+    bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+except:
+    print("BMP280 missing or damaged")
+    bmpMissing = True
+try:
+    aht20 = adafruit_ahtx0.AHTx0(i2c)
+except:
+    print("AHT20 missing or damaged")
+    ahtMissing = True
+try:
+    lps22 = adafruit_lps2x.LPS22(i2c)
+except:
+    print("LPS22 missing or damaged")
+    lpsMissing = True
+try:
+    sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
+except:
+    print("SGP30 missing or damaged")
+    sgpMissing = True
+
+print("I2C addresses found:",[hex(device_address) for device_address in i2c.scan()],)
 
 #LED CIRCUIT SETUP
 print("Setting up GPIO17 for the LED")
 led = digitalio.DigitalInOut(board.D17)
 led.direction = digitalio.Direction.OUTPUT
-print("I2C addresses found:",[hex(device_address) for device_address in i2c.scan()],)
 
 #THE MAIN FUNCTION
 try:
     print("Initiating Readings...")
     while True:
         #DECLARE READIINGS
-        enviro_temp = float("{:.2f}".format(bmp280.temperature))
-        enviro_humidity = float("{:.2f}".format(aht20.relative_humidity))
-        enviro_CO2 = float("{:.2f}".format(sgp30.eCO2))
-        enviro_pressure = float("{:.3f}".format(lps22.pressure))
+        if bmpMissing:
+            enviro_temp = 21.00
+        else:
+            enviro_temp = float("{:.2f}".format(bmp280.temperature))
+        if ahtMissing:
+            enviro_humidity = 15.00
+        else:
+            enviro_humidity = float("{:.2f}".format(aht20.relative_humidity))
+        if sgpMissing:
+            enviro_CO2 = 400.00
+        else:
+            enviro_CO2 = float("{:.2f}".format(sgp30.eCO2))
+        if lpsMissing:
+            enviro_pressure = 930.000
+        else:
+            enviro_pressure = float("{:.3f}".format(lps22.pressure))
         
         #DATA READINGS FOR INDIVIDUAL TESTING
         db.child("Weather Station Reading").child("temperature_reading").set(enviro_temp)
