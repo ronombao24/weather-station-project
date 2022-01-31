4ac55b19dca6c275162cb3aaa82d82b39dec51d8
TJStave <n01286513@humbermail.ca>
2022-01-31 11:46:13 -0500

    began refactoring

--- capstone_project.py
+++ capstone_project.py
@@ -11,42 +11,46 @@ import adafruit_bmp280
 import adafruit_sgp30
 import adafruit_ahtx0
 
-#FIREBASE CONFIG
-config = {
-    "apiKey" : "iF4wAXdjSQGAJiu11cqAlIkd3vHs95I0Psa6fw0w",
-    "authDomain": "ceng317weatherstationproject.firebaseapp.com",
-    "databaseURL" : "https://ceng317weatherstationproject-default-rtdb.firebaseio.com/",
-    "storageBucket" : "ceng317weatherstationproject.appspot.com"
-}
+def firebase_init():
+    #FIREBASE CONFIG
+    config = {
+        "apiKey" : "iF4wAXdjSQGAJiu11cqAlIkd3vHs95I0Psa6fw0w",
+        "authDomain": "ceng317weatherstationproject.firebaseapp.com",
+        "databaseURL" : "https://ceng317weatherstationproject-default-rtdb.firebaseio.com/",
+        "storageBucket" : "ceng317weatherstationproject.appspot.com"
+    }
 
-#DECLARE FIREBASE OBJECT
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
+    #DECLARE FIREBASE OBJECT
+    firebase = pyrebase.initialize_app(config)
+    database = firebase.database()
+    return database
+
+db = firebase_init()
 
 #SENSOR DECLARATIONS
-sensorMissing = {"lps": [False, 2], "bmp": [False, 3], "sgp": [False, 4], "aht": [False, 5]}
+sensor_missing = {"lps": [False, 2], "bmp": [False, 3], "sgp": [False, 4], "aht": [False, 5]}
 
 i2c = busio.I2C(board.SCL, board.SDA)
 try:
     lps22 = adafruit_lps2x.LPS22(i2c)
 except:
     print("LPS22 missing or damaged")
-    sensorMissing["lps"][0] = True
+    sensor_missing["lps"][0] = True
 try:
     bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 except:
     print("BMP280 missing or damaged")
-    sensorMissing["bmp"][0] = True
+    sensor_missing["bmp"][0] = True
 try:
     sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
 except:
     print("SGP30 missing or damaged")
-    sensorMissing["sgp"][0] = True
+    sensor_missing["sgp"][0] = True
 try:
     aht20 = adafruit_ahtx0.AHTx0(i2c)
 except:
     print("AHT20 missing or damaged")
-    sensorMissing["aht"][0] = True
+    sensor_missing["aht"][0] = True
 
 print("I2C addresses found:",[hex(device_address) for device_address in i2c.scan()],)
 
@@ -60,19 +64,19 @@ try:
     print("Initiating Readings...")
     while True:
         #DECLARE READIINGS
-        if sensorMissing["lps"][0]:
+        if sensor_missing["lps"][0]:
             enviro_pressure = 930.000
         else:
             enviro_pressure = float("{:.3f}".format(lps22.pressure))
-        if sensorMissing["bmp"][0]:
+        if sensor_missing["bmp"][0]:
             enviro_temp = 21.00
         else:
             enviro_temp = float("{:.2f}".format(bmp280.temperature))
-        if sensorMissing["sgp"][0]:
+        if sensor_missing["sgp"][0]:
             enviro_CO2 = 400.00
         else:
             enviro_CO2 = float("{:.2f}".format(sgp30.eCO2))
-        if sensorMissing["aht"][0]:
+        if sensor_missing["aht"][0]:
             enviro_humidity = 15.00
         else:
             enviro_humidity = float("{:.2f}".format(aht20.relative_humidity))
@@ -90,23 +94,23 @@ try:
         print("Humidity: {} %rH". format(enviro_humidity))
 
         #STATUS LED
-        for sensorName in sensorMissing:
-            if sensorMissing[sensorName][0]:
-                numBlinks = sensorMissing[sensorName][1]
+        for sensor_name in sensor_missing:
+            if sensor_missing[sensor_name][0]:
+                num_blinks = sensor_missing[sensor_name][1]
                 break
         else:
-            numBlinks = 1
+            num_blinks = 1
         
-        offTime = 3.0
+        off_time = 3.0
         i = 0
-        while i < numBlinks:
+        while i < num_blinks:
             led.value = True
             time.sleep(0.2)
             led.value = False
             time.sleep(0.2)
-            offTime -= 0.4
+            off_time -= 0.4
             i += 1
-        time.sleep(offTime)
+        time.sleep(off_time)
 
 except KeyboardInterrupt:
     print("\nProgram is shut off by CTRL+C command. Cleaning all GPIO connections.\n")
