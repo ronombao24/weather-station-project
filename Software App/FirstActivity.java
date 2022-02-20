8df798e80083780059c35356135101587ac4478c
sarthakthakkar <71234720+sarthakthakkar@users.noreply.github.com>
2022-02-20 16:40:00 -0500

    Add files via upload

--- Software App/FirstActivity.java
+++ Software App/FirstActivity.java
@@ -9,6 +9,6 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_splash);
     }
 }
--- Software App/MainActivity.java
+++ Software App/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.weatherdetector;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+
+    EditText name;
+    EditText email;
+    EditText password;
+    EditText reenter;
+    Button signUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstName = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signup = findViewById(R.id.signup);
 
         button = (Button) findViewById(R.id.app_button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +53,30 @@ public class MainActivity extends AppCompatActivity {
         Button signup = findViewById(R.id.signup_button);
         //Button app = findViewById(R.id.app_button);
         Button first = findViewById(R.id.first_button);
+
+        signup.setOnClickListener(new View.OnClickListener){
+            @Override
+                    public void onClick(View view)
+        {
+            checkingTheData;
+        }
+    }
+
+
+    void checkingTheData(){
+        if(empty(name))
+        {
+            //Toast is used to display a message when user enters nothing or something wrong
+            Toast toast= Toast.makeText(this, "Enter your first name to register", Toast.LENGTH_SHORT)
+            toast.show();
+        }
+
+        if(email(email) == false)
+        {
+            email.setError("Invalid email. Enter the correct email");
+        }
+    }
+
 
         //temp_btn.setOnClickListener (view -> startActivity(new Intent(MainActivity.this, TemperatureReader.class)));
 
