43c0343dd72826f30055aa8e3b25e91681ee6bf7
sarthakthakkar <71234720+sarthakthakkar@users.noreply.github.com>
2022-02-20 10:40:48 -0500

    Add files via upload

--- Software App/AirPressureReader.java
+++ Software App/AirPressureReader.java
@@ -6,10 +6,9 @@ import android.os.Bundle;
 
 public class AirPressureReader extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_air_pressure_reader);
-        //THIS IS TEST
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_air_pressure_reader);
+  }
 }
